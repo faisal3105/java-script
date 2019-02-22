6ea222ac6e73a6d35295df709c601d2ed28e1083
Faisal Hussain <46702227+faisal3105@users.noreply.github.com>
2019-02-22 18:07:00 +0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,16 @@
+function myfunction() {
+ document.getElementById("btn").innerHTML = "Click Here";  
+}
+function myfunction1() {
+ document.getElementById("text").innerHTML = "How are you?";
+}
+    
+function array1() {
+   var names = [
+       "Faisal",
+       "Hina",
+       "Ayesha"
+   ];
+ document.getElementById("text1").innerHTML = "JavaScript Display Possibilities JavaScript can display data in different ways: Writing into an HTML element, using innerHTML. Writing into the HTML output using document.write().Writing into an alert box, using window.alert().Writing into the browser console, using console.log().";
+ document.getElementById("arraylist").innerHTML = names;
+}
